a29c26d7663e947bd3d6fb1f7546d90f4f05f47f
Waleed OMEGA <waleedomega@hotmail.com>
2022-08-18 15:35:10 +0200

    last changes

--- CS Course/5-1/main.cpp
+++ CS Course/5-1/main.cpp
@@ -2,6 +2,42 @@
 
 using namespace std;
 
+class Date{
+private:
+    int day,month,year;
+public:
+    Date (int day,int month,int year){
+        day=day;
+        month=month;
+        year=year;
+    }
+
+    int getDay(){
+    return day;
+    }
+    int getMonth(){
+    return month;
+    }
+    int getYear(){
+    return year;
+    }
+    void setDay(int day){
+    day=day;
+    }
+    void setMonth(int month){
+    month=month;
+    }
+    void setYear(int year){
+    year=year;
+    }
+    void setDate (int day,int month,int year){
+        day=day;
+        month=month;
+        year=year;
+    }
+};
+
+
 int main()
 {
     cout << "Hello world!" << endl;
