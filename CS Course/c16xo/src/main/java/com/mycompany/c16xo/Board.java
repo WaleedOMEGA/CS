6bc559a4c2a1ee425e5f300a9a28aba6bb027cb0
Waleed OMEGA <waleedomega@hotmail.com>
2022-09-28 14:00:41 +0200

    updated

--- CS Course/c16xo/src/main/java/com/mycompany/c16xo/Board.java
+++ CS Course/c16xo/src/main/java/com/mycompany/c16xo/Board.java
@@ -9,5 +9,10 @@ package com.mycompany.c16xo;
  * @author Professional
  */
 public class Board {
+    char[][] arr;
+
+    public Board(char[][] arr) {
+        this.arr = new char [][]{{'1','2','3'},{'4','5','6'},{'7','8','9'}};
+    }
     
 }
